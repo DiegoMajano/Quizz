1a068dd23cc14fcbf9ebc05a75061b9698308da8
DiegoMajano <dmajano@gmail.com>
2024-08-27 01:14:09 -0600

    agregando las funciones para mostrar las preguntas del quiz

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ import { mostrarPregunta } from "./src/components/script";
 setInitialMode();
 
 function main(){
+
+
+  // generar un codigo para verificar que tematica selecciono el usuario para el quiz y asi seleccionar las pregunas correspondientes
+  
+
   mostrarPregunta(0)
 }
 
--- src/components/script.js
+++ src/components/script.js
@@ -2,10 +2,9 @@ import { datajs } from "../../javascript/data/data-javascript";
 
 let contador = 0
 let score = 0
-let numeroPregunta = 1
 
 const btnNext = document.querySelector("#btn-next")    
-const totalText = document.querySelector('.progress')
+const totalText = document.querySelector('.n-preguntas')
 
 // agregando un event listener
 
@@ -16,12 +15,10 @@ btnNext.addEventListener('click', ()=>{
         mostrarPregunta(contador)
     } else{
         console.log('Quiz completado');
-        
-    }
-
+    }   
 })
 
-const opcionesBox = document.querySelector("#options-list")
+const opcionesBox = document.querySelector(".options-list")
 
 // mostrar pregunta en el contenedor correspondiente
 export function mostrarPregunta(index){
@@ -31,30 +28,58 @@ export function mostrarPregunta(index){
     
     preguntaText.textContent = `${index+1}. ${datajs[index].pregunta}`
 
-    let options = `<button id="A" class="option">
-                    <div class="option-box">A</div>
-                    ${datajs[index].opciones[0]}
-                </button>
-                <button id="B" class="option">
-                    <div class="option-box">B</div>
-                    ${datajs[index].opciones[1]}
-                </button>
-                <button id="C" class="option">
-                    <div class="option-box">C</div>
-                    ${datajs[index].opciones[2]}
-                </button>
-                <button id="D" class="option">
-                    <div class="option-box">D</div>
-                    ${datajs[index].opciones[3]}
-                </button>`;
+    let options = `
+    
+    <button id="A" class="option">A. ${datajs[index].opciones[0]}</button>
+    <button id="B" class="option">B. ${datajs[index].opciones[1]}</button>
+    <button id="C" class="option">C. ${datajs[index].opciones[2]}</button>
+    <button id="D" class="option">D. ${datajs[index].opciones[3]}</button>
+    `;
     
     opcionesBox.innerHTML = options;
 
+    const option = document.querySelectorAll('.option')
+    option.forEach(op => {
+        op.addEventListener('click', function() {
+            // Llama a la función con 'this' que representa el elemento clickeado
+            seleccionarOpcion(this);
+        });
+    });
+}
+
+function seleccionarOpcion(respuesta){
+    let respuestaSeleccionada = respuesta.textContent.slice(3,respuesta.length);
+    let respuestaCorrecta = datajs[contador].correcta
+    
+    
+    // console.log(respuestaCorrecta);    
+         
+    if(respuestaSeleccionada == respuestaCorrecta){
+        respuesta.classList.add('correcta')
+        score++;
+        actualizarScore();
+    } else{
+        respuesta.classList.add('incorrecta')
+
+        for(let i = 0; i < opcionesBox.children.length; i++){
+            if(opcionesBox.children[i].textContent == respuestaCorrecta){
+                opcionesBox.children[i].classList.add('correcta')
+            } 
+        }
+
+    }   
+
+    for(let i = 0; i < opcionesBox.children.length; i++){
+        opcionesBox.children[i].classList.add('desactivado')
+    }
+    
 }
 
 function actualizarContador(index){
-
     totalText.innerHTML = ` ${index+1} de ${datajs.length} preguntas `
-    console.log(` <p> ${index+1} de ${datajs.length} preguntas </p> `);
-    
+}
+
+function actualizarScore(){
+    const scoreText = document.querySelector('.score')
+    scoreText.textContent = `Puntuación: ${score} / ${datajs.length}`
 }
