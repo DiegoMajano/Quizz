34617ab669c37c71fd9f63fb086e4b3d2b913048
Carlos Flores <carlosflores3108@hotmail.com>
2024-08-27 11:31:17 -0600

    Agregando banco de preguntas HTML

--- javascript/data/data-html.js
+++ javascript/data/data-html.js
@@ -1,3 +1,52 @@
 export const dataHtml = [
-    {}
-]
+    {
+        pregunta: '¿Cuál es la etiqueta correcta para incluir un archivo CSS externo en un documento HTML?',
+        opciones: ['<style>', '<css>', '<link>', '<stylesheet>'],
+        correcta: '<link>'
+    },
+    {
+        pregunta: '¿Qué atributo de la etiqueta <form> se usa para especificar el método de envío?',
+        opciones: ['method', 'action', 'type', 'enctype'],
+        correcta: 'method'
+    },
+    {
+        pregunta: '¿Cuál es la etiqueta utilizada para definir una sección de navegación en HTML5?',
+        opciones: ['<nav>', '<navigation>', '<menu>', '<section>'],
+        correcta: '<nav>'
+    },
+    {
+        pregunta: '¿Cómo se establece un atributo de un elemento en HTML?',
+        opciones: ['=', '{}', '()', '[]'],
+        correcta: '='
+    },
+    {
+        pregunta: '¿Cuál es la etiqueta correcta para definir un área de texto multilínea en un formulario?',
+        opciones: ['<input>', '<textarea>', '<text>', '<field>'],
+        correcta: '<textarea>'
+    },
+    {
+        pregunta: '¿Qué atributo se utiliza para especificar el texto alternativo de una imagen en HTML?',
+        opciones: ['alt', 'title', 'desc', 'label'],
+        correcta: 'alt'
+    },
+    {
+        pregunta: '¿Cuál es la diferencia entre <div> y <span> en HTML?',
+        opciones: ['Bloque / En línea', 'Texto / Multimedia', 'Tablas / Formularios', 'Son iguales'],
+        correcta: 'Bloque / En línea'
+    },
+    {
+        pregunta: '¿Qué atributo de la etiqueta <iframe> se usa para especificar el origen del contenido',
+        opciones: ['src', 'href', 'source', 'link'],
+        correcta: 'src'
+    },
+    {
+        pregunta: '¿Cómo se define una estructura de documento HTML5 semántico para un artículo de blog?',
+        opciones: ['<article>', '<blog>', '<section>', '<post>'],
+        correcta: '<article>'
+    },
+    {
+        pregunta: '¿Qué etiqueta se utiliza para agrupar un conjunto de controles en un formulario HTML?',
+        opciones: ['<fieldset>', '<group>', '<container>', '<box>'],
+        correcta: '<fieldset>'
+    }
+];
