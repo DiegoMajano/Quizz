04ed9e98fe8d74a3d850173480493153e68e346e
Kevin7365 <141382696+Kevin7365@users.noreply.github.com>
2024-08-29 23:27:06 -0600

    agregando arreglo con preguntas css

--- javascript/data/data-css.js
+++ javascript/data/data-css.js
@@ -1,3 +1,83 @@
 export const dataCss = [
-    {}
-]
+    {
+        pregunta: '¿Qué significa CSS en el desarrollo web?',
+        opciones: [ 'Computer Style Sheets',
+                    'Cascading Style Sheets', 
+                    'Content Structure System', 
+                    
+                    'Creality Styling Solution'],
+        correcta: 'Cascading Style Sheets'
+    },
+    {
+        pregunta: '¿Cuál es la función de CSS en el desarrollo web?',
+        opciones: [ 'Su función es programar la lógica de la página web', 
+                    'Su función es controlar la presentación y el diseño de páginas web', 
+                    'Su función es organizar el contenido en la página',
+                    'Su función es añadir efectos visuales avanzados'],
+        correcta: 'Su función es controlar la presentación y el diseño de páginas web'
+    },
+    {
+        pregunta: '¿Cuál es la diferencia entre Margin y Padding?',
+        opciones: [ 'Margin es el espacio interno, mientras que padding es el espacio externo.', 
+                    'Margin es el espacio entre elementos, y padding es el espacio dentro del elemento', 
+                    'Margin afecta el tamaño del elemento, mientras que padding no',
+                    'No hay diferencia, son propiedades sinónimas'],
+        correcta: 'Margin es el espacio entre elementos, y padding es el espacio dentro del elemento'
+    },
+    {
+        pregunta: '¿Qué es el modelo de caja (box model)?',
+        opciones: [ 'Un sistema para crear diseños basados en cajas', 
+                    'Una técnica de centrar elementos en la página', 
+                    'Un concepto que describe cómo se realiza cada elemento HTML',
+                    'Un método para crear animaciones en CSS'],
+        correcta: 'Un concepto que describe cómo se realiza cada elemento HTML'
+    },
+    {
+        pregunta: '¿Cómo se implementan las especificidades en CSS?',
+        opciones: [ 'Se implementa usando !important;', 
+                    'Se calcula basándose en el tipo de selector;', 
+                    'Simplemente usando z-Index; ',
+                    'Se calcula basándose en el orden de los estilos; '],
+        correcta: 'Se calcula basándose en el tipo de selector;'
+    },
+    {
+        pregunta: '¿Qué son los pseudoelementos y para qué nos sirve?',
+        opciones: [ 'Elementos HTML especiales que sólo existen en CSS', 
+                    'Selectores que usan para elementos ocultos', 
+                    'Partes de un elemento que pueden ser utilizadas como ::before o ::after',
+                    'Clases predefinidas en CSS para estilos comunes.'],
+        correcta: 'Partes de un elemento que pueden ser utilizadas como ::before o ::after'
+    },
+    {
+        pregunta: '¿Cuál de las siguientes NO es una técnica para crear diseños responsive en CSS?',
+        opciones: [ 'Usar media queries', 
+                    'Utilizar unidades relativas, como em y rem', 
+                    'Implementar el Flexbox y Grid.',
+                    'Fijar todos los tamaños en pixeles.'],
+        correcta: 'Fijar todos los tamaños en pixeles.'
+    },
+    {
+        pregunta: '¿Qué propiedades CSS se usa para hacer que una imagen sea responsive?',
+        opciones: [ 'img-responsive', 
+                    'max-width: 100%', 
+                    'width: auto',
+                    'responsive: true'],
+        correcta: 'max-width: 100%'
+    },
+    {
+        pregunta: '¿Cuál es la diferencia entre usar em y rem para tamaños de fuente',
+        opciones: [ 'em basado en elemento actual, rem en elemento raíz.', 
+                    'em basado en elemento padre, rem en elemento raíz.', 
+                    'em para anchuras, rem para alturas',
+                    'No hay diferencia, son intercambiables.'],
+        correcta: 'Partes de un elemento que pueden ser utilizadas como ::before o ::after'
+    },
+    {
+        pregunta: '¿Cómo se establece una imagen como fondo de un elemento en CSS?',
+        opciones: [ 'background-image', 
+                    'image-background', 
+                    'background-img ',
+                    'img-background.'],
+        correcta: 'background-image'
+    }
+];
